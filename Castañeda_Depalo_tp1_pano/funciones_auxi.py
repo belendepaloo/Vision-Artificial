d697c7b771ab23a0388117c93f74b297f26c9aa7
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-09-07 12:52:44 -0300

    3.3 para preguntar

--- Castañeda_Depalo_tp1_pano/funciones_auxi.py
+++ Castañeda_Depalo_tp1_pano/funciones_auxi.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 #ANMS: Adaptive Non-Maximal Suppression
-def anms(corners, num_corners=100):
+def anms(corners, num_corners=500):
     if len(corners) <= num_corners:
         return corners
 
@@ -23,11 +23,13 @@ def anms(corners, num_corners=100):
 
     return corners[selected_indices]
 
+
+
 #Función para imprimir los keypoints ANMS
 def imprimir_anms(pts, img):
     #Imprimo los keypoints SIFT antes y despues de ANMS
     print(f"Keypoints antes de ANMS: {len(pts)}")
-    keypoints_anms = anms(pts, num_corners=100)
+    keypoints_anms = anms(pts, num_corners=500)
     print(f"Keypoints despues de ANMS: {len(keypoints_anms)}")
 
     #Mostrar los puntos ANMS sobre fondo blanco
@@ -37,3 +39,23 @@ def imprimir_anms(pts, img):
     plt.title("Keypoints despues de ANMS (solo puntos)")
     plt.axis('off')
     plt.show()
+    return keypoints_anms
+
+#Función para hacer match entre puntos ANMS y keypoints originales
+def match_pts_to_keypoints(pts_anms, keypoints_all):
+    keypoints_red = []
+    # recorro cada punto (x, y) que fue seleccionado por ANMS
+    for pt_anms in pts_anms:
+        # recorro todos los keypoints originales de la imagen
+        for kp in keypoints_all:
+            # si las coordenadas del KeyPoint coinciden con las del punto ANMS, lo consideramos equivalente
+            if tuple(map(int, kp.pt)) == tuple(map(int, pt_anms)):
+                keypoints_red.append(kp)
+                break
+    return keypoints_red
+
+#Función para extraer descriptores de los keypoints filtrados
+def extraer_descriptores(keypoints_all, keypoints_filtered, descriptors_all):
+    # extraigo los descriptores correspondientes a los keypoints filtrados.
+    indices = [keypoints_all.index(kp) for kp in keypoints_filtered]
+    return descriptors_all[indices]
